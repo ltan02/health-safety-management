18de468061c4c326be64b9c5388db98f3423253d
Billy <ryotabilly@gmail.com>
2024-02-15 22:14:51 -0800

    add theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,44 @@ import Root from "./routes/root";
 import Admin from "./pages/admin";
 import Incident from "./pages/incident";
 import Report from "./pages/report";
+import { COLORS } from "./constants";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    fontFamily: "Roboto",
+  },
+  palette: {
+    primary: {
+      light: COLORS.LIGHT, //set these later
+      main: COLORS.PRIMARY,
+      dark: COLORS.DARK,
+      contrastText: "black"
+    },
+    secondary: {
+      light: "#DEDEDE", //set these later
+      main: "#7D7D7D",
+      dark: "#464646",
+    },
+  },
+});
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Root />} />
-          <Route path="admin" element={<Admin />} />
-          <Route path="incident" element={<Incident />} />
-          <Route path="report" element={<Report />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="admin" element={<Admin />} />
+            <Route path="incident" element={<Incident />} />
+            <Route path="report" element={<Report />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </>
   );
