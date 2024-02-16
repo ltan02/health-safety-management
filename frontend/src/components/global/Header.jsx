ffe83db0eabbaab356350c50348db98d4bd12ccd
Billy <ryotabilly@gmail.com>
2024-02-15 22:15:48 -0800

    add sidebar

--- frontend/src/components/global/Header.jsx
+++ frontend/src/components/global/Header.jsx
@@ -5,42 +5,84 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Sidebar from "./Sidebar";
 
 function Header() {
+  const drawerWidth = 300;
   const navigate = useNavigate();
+  const [sidebarOpen, setSidebarOpen] = useState(false); // Assuming you want the sidebar to be closed initially
 
   const directTo = (path) => () => {
     navigate(path);
-  }
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="primary" maxWidth>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            AppName
-          </Typography>
-          <Container
+      <AppBar position="static" color="primary">
+        <Toolbar
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Sidebar
+            drawerWidth={drawerWidth}
+            isOpen={sidebarOpen}
+            handleSidebarToggle={setSidebarOpen} // Assuming Sidebar manages its own open/close state
+          />
+          <Box
             sx={{
               display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexGrow: 1,
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                // TODO: comment  this if we don't want to push the header to the right when the sidebar is open
+                marginLeft: sidebarOpen ? `${drawerWidth}px` : "0",
+                transition: "margin 300ms ease-out",
+              }}
+            >
+              AppName
+            </Typography>
+            <Container
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "2rem",
+                padding: "0px",
+              }}
+            >
+              <Button color="inherit" onClick={directTo("/admin")}>
+                Admin
+              </Button>
+              <Button color="inherit" onClick={directTo("/incident")}>
+                Incident
+              </Button>
+              <Button color="inherit" onClick={directTo("/report")}>
+                Report
+              </Button>
+            </Container>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
               alignItems: "center",
               justifyContent: "center",
-              gap: "6rem",
             }}
-            style={{ padding: "0px" }}
           >
-            <Button color="inherit" onClick={directTo("/admin")}>
-              Admin
-            </Button>
-            <Button color="inherit" onClick={directTo("/incident")}>
-              Incident
-            </Button>
-            <Button color="inherit" onClick={directTo("/report")}>
-              Report
-            </Button>
-          </Container>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Logout</Button>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Logout</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
---
+++ frontend/src/components/global/Sidebar.jsx
@@ -0,0 +1,118 @@
+import { useEffect } from "react";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import List from "@mui/material/List";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Typography from "@mui/material/Typography";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { PAGE_TYPE, SIDEBAR_CONTENTS, COLORS } from "../../constants";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
+function Sidebar({ isOpen, drawerWidth, handleSidebarToggle }) {
+  const toggleDrawer = (open) => () => handleSidebarToggle(open);
+  const [pageType, setPageType] = useState(PAGE_TYPE.ADMIN);
+  let location = useLocation();
+
+  useEffect(() => {
+    setPageType(location.pathname.substring(1));
+  }, [location]);
+  
+
+  const renderList = (items) => (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2rem",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "2rem",
+        }}
+      >
+        <AccountCircleIcon
+          style={{
+            fontSize: 84,
+            padding: "0px",
+          }}
+        />
+      </Box>
+      <List
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          width: "240px",
+        }}
+      >
+        {items.map((text) => (
+          <Button
+            variant="contained"
+            key={text}
+            sx={{
+              width: "100%",
+            }}
+          >
+            <Typography fontSize={"1.2rem"}>{text}</Typography>
+          </Button>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <IconButton onClick={toggleDrawer(!isOpen)}>
+        {isOpen ? <ArrowBackIosIcon /> : <ArrowForwardIosIcon />}
+      </IconButton>
+      <SwipeableDrawer
+        ModalProps={{ keepMounted: true }}
+        anchor="left"
+        open={isOpen}
+        onClose={toggleDrawer(false)}
+        variant="persistent"
+        sx={{
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+          },
+        }}
+      >
+        <IconButton
+          onClick={toggleDrawer(!isOpen)}
+          sx={{
+            marginLeft: "auto",
+          }}
+        >
+          {isOpen ? <ArrowBackIosIcon /> : <ArrowForwardIosIcon />}
+        </IconButton>
+        <Box
+          role="presentation"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "2rem",
+            padding: "2rem",
+          }}
+        >
+          {renderList(SIDEBAR_CONTENTS[pageType])}
+        </Box>
+      </SwipeableDrawer>
+    </Box>
+  );
+}
+
+export default Sidebar;
