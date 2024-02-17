201781407e95b5bd27e4c4a5694f0f2d745ec1f2
Billy <ryotabilly@gmail.com>
2024-02-16 23:57:52 -0800

    add env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      strictPort: true,
+      host: true,
+    },
+    define: {
+      "process.env": env,
+    },
+  };
 });
