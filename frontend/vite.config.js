3e0ef5926070ff0370e55fffff45784a5fb8bfce
Billy <ryotabilly@gmail.com>
2024-02-18 16:52:21 -0800

    remove

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       port: 3000,
       strictPort: true,
       host: true,
-    },
-    define: {
-      "process.env": env,
     },
   };
 });
