45fbc7393f410912c379711639ee0af72b46db9c
Billy <ryotabilly@gmail.com>
2024-02-28 16:19:39 -0800

    add header for workflow management

--- frontend/src/pages/admin/management/index.jsx
+++ frontend/src/pages/admin/management/index.jsx
@@ -8,40 +8,34 @@ import ReactFlow, {
   addEdge,
 } from "reactflow";
 import "reactflow/dist/style.css";
-import {EMPLOYEE_COLUMNS, ADMIN_COLUMNS} from "../../../constants/board";
+import { EMPLOYEE_COLUMNS } from "../../../constants/board";
 
 import { Container } from "@mui/material";
-const initialNodes = [
-  {
-    id: EMPLOYEE_COLUMNS[0].id,
-    position: { x: 0, y: 0 },
-    data: { label: EMPLOYEE_COLUMNS[0].title },
-  },
-  {
-    id: EMPLOYEE_COLUMNS[1].id,
-    position: { x: 0, y: 100 },
-    data: { label: EMPLOYEE_COLUMNS[1].title },
-  },
-  {
-    id: EMPLOYEE_COLUMNS[2].id,
-    position: { x: 0, y: 200 },
-    data: { label: EMPLOYEE_COLUMNS[2].title },
-  },
-  {
-    id: EMPLOYEE_COLUMNS[3].id,
-    position: { x: 0, y: 300 },
-    data: { label: EMPLOYEE_COLUMNS[3].title },
-  },
-];
+import ManagementHeader from "./ManagementHeader";
+let y_coordinate = 0;
+const initialNode = {
+  id: "start",
+  type: "input",
+  position: { x: 0, y:0 },
+  data: { label: "Start" },
+}
+
+const initialNodes = [initialNode, ...EMPLOYEE_COLUMNS.map((item) => {
+  return {
+    id: item.id,
+    position: { x: 0, y: (y_coordinate += 100) },
+    data: { label: item.title },
+  };
+})]
 
 const initialEdges = [
-  { id: "e1", source: EMPLOYEE_COLUMNS[0].id, target: EMPLOYEE_COLUMNS[1].id },
-  { id: "e2", source: EMPLOYEE_COLUMNS[1].id, target: EMPLOYEE_COLUMNS[2].id },
+  { id: "e1", source: "start", target: EMPLOYEE_COLUMNS[0].id },
 ];
 
 function AdminManagement() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  console.log(nodes);
 
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
@@ -49,7 +43,8 @@ function AdminManagement() {
   );
 
   return (
-    <Container style={{ width: "800px", height: "700px" }}>
+    <Container style={{ width: "2000px", height: "600px"}}>
+      <ManagementHeader columns={EMPLOYEE_COLUMNS} />
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -58,9 +53,9 @@ function AdminManagement() {
         onConnect={onConnect}
         fitView
       >
-        {/* <MiniMap />
+        <MiniMap />
         <Controls />
-        <Background /> */}
+        <Background />
       </ReactFlow>
     </Container>
   );
