c25adae65ac3637d127750626e264e8b906698c9
Billy <ryotabilly@gmail.com>
2024-03-05 13:54:31 -0800

    add repo

---
+++ backend/src/main/java/com/teamadc/backend/repository/impl/TransitionRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.teamadc.backend.repository.impl;
+
+import com.teamadc.backend.model.Transition;
+import com.teamadc.backend.repository.GenericRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TransitionRepositoryImpl extends AbstractFirestoreRepository<Transition> implements GenericRepository<Transition> {
+    public TransitionRepositoryImpl() {
+        super(Transition.class);
+    }
+
+    @Override
+    protected String getCollectionName() {
+        return "Transition";
+    }
+}
