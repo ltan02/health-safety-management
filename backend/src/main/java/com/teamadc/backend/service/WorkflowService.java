a8b200df90c7b5f75adf520e451d56a7a3d32b01
Billy <ryotabilly@gmail.com>
2024-03-05 13:54:55 -0800

    add workflow manager endpoints and services

--- backend/src/main/java/com/teamadc/backend/service/WorkflowService.java
+++ backend/src/main/java/com/teamadc/backend/service/WorkflowService.java
@@ -1,24 +1,32 @@
 package com.teamadc.backend.service;
 
 import com.teamadc.backend.model.Workflow;
+import com.teamadc.backend.model.State;
+import com.teamadc.backend.model.Transition;
 import com.teamadc.backend.repository.GenericRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class WorkflowService {
-
     private final GenericRepository<Workflow> workflowRepository;
+    private final GenericRepository<State> stateRepository;
+    private final GenericRepository<Transition> transitionRepository;
 
     @Autowired
-    public WorkflowService(GenericRepository<Workflow> workflowRepository) {
+    public WorkflowService(GenericRepository<Workflow> workflowRepository, GenericRepository<State> stateRepository, GenericRepository<Transition> transitionRepository) {
         this.workflowRepository = workflowRepository;
+        this.stateRepository = stateRepository;
+        this.transitionRepository = transitionRepository;
     }
 
     public Workflow createOrUpdateWorkflow(Workflow workflow) throws InterruptedException, ExecutionException {
+        workflow.setTransitions(List.of());
+        workflow.setStates(List.of());
         return workflowRepository.save(workflow);
     }
 
@@ -33,4 +41,41 @@ public class WorkflowService {
     public List<Workflow> getWorkflows() throws InterruptedException, ExecutionException {
         return workflowRepository.findAll();
     }
+
+    public void addState(String workflowId, State state ) throws InterruptedException, ExecutionException {
+        Workflow workflow = workflowRepository.findById(workflowId);
+        List<State> states = workflow.getStates();
+        stateRepository.save(state);
+        states.add(state);
+        workflow.setStates(states);
+        workflowRepository.save(workflow);
+    }
+
+    public void deleteState(String workflowId, String stateId) throws InterruptedException, ExecutionException {
+        Workflow workflow = workflowRepository.findById(workflowId);
+        List<State> states = workflow.getStates();
+        stateRepository.deleteById(stateId);
+        states.removeIf(state -> state.getId().equals(stateId));
+        workflow.setStates(states);
+        workflowRepository.save(workflow);
+    }
+
+    public void addTransition(String workflowId, Transition transition) throws InterruptedException, ExecutionException {
+        Workflow workflow = workflowRepository.findById(workflowId);
+        List<Transition> flows = workflow.getTransitions();
+        transitionRepository.save(transition);
+        flows.add(transition);
+        workflow.setTransitions(flows);
+        workflowRepository.save(workflow);
+    }
+
+    public void deleteTransition(String workflowId, String flowId) throws InterruptedException, ExecutionException {
+        Workflow workflow = workflowRepository.findById(workflowId);
+        List<Transition> flows = workflow.getTransitions();
+        transitionRepository.deleteById(flowId);
+        flows.removeIf(f -> f.getId().equals(flowId));
+        workflow.setTransitions(flows);
+        workflowRepository.save(workflow);
+    }
+    
 }
