582eb556a3c3576ba2c75e0887f1ab9541ba6f1a
Billy <ryotabilly@gmail.com>
2024-03-07 14:10:51 -0800

    add tabs

---
+++ frontend/src/pages/admin/form/FormCustomizationModal.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Paper, TextField, Modal } from "@mui/material";
+import FormTab from "./FormTab";
+import PreviewForm from "./PreviewForm";
+
+function FormCustomizationModal({ open, handleClose }) {
+  return (
+    <Modal open={open} onClose={handleClose}>
+        <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: "auto", maxWidth: "90vw", bgcolor: "background.paper", boxShadow: 24, p: 4, borderRadius: 2 }}>
+            <Paper elevation={3} sx={{ width: "80vw" }}>
+                <FormTab labels={["Preview", "Customize", "Edit"]}>
+                    <PreviewForm />
+                    <PreviewForm />
+                    <PreviewForm />
+                </FormTab>
+            </Paper>
+        </Box>
+    </Modal>
+  );
+}
+
+export default FormCustomizationModal;
