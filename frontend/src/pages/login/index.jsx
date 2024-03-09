39ac48be833291ea15bede9454468a31c8566ee8
shrsin3 <singhaniamanushree3@gmail.com>
2024-03-08 16:38:56 -0800

    Login Page Design

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { Container, Card, CardContent, Typography, TextField, Button } from "@mui/material";
 import { useAuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import pwcLogo from "../../assets/pwcLogo.svg"
+
 
 function Login() {
     const navigate = useNavigate();
@@ -18,47 +20,66 @@ function Login() {
     const handleSubmit = () => {
         signIn(email, password);
     };
+
+    useEffect(() => {
+        document.body.style.backgroundColor = '#EB8C00';
+        return () => {
+            document.body.style.backgroundColor = '';
+        };
+    }, []);
+
     return (
-        <Container maxWidth="sm" style={{ marginTop: "100px" }}>
-            <Card>
-                <CardContent>
-                    <Typography variant="h5" component="h2" style={{ marginBottom: "20px" }}>
-                        Login
-                    </Typography>
-                    <form noValidate autoComplete="off">
-                        <TextField
-                            label="Email"
-                            type="email"
-                            fullWidth
-                            required
-                            margin="normal"
-                            variant="outlined"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <TextField
-                            label="Password"
-                            type="password"
-                            fullWidth
-                            required
-                            margin="normal"
-                            variant="outlined"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <Button
-                            onClick={handleSubmit}
-                            color="primary"
-                            variant="contained"
-                            fullWidth
-                            style={{ marginTop: "20px" }}
-                        >
-                            Login
-                        </Button>
-                    </form>
-                </CardContent>
-            </Card>
-        </Container>
+            <Container maxWidth="xs" style={{ marginTop: "100px"}}>
+                <Card>
+                    <CardContent style={{paddingRight: "1.5rem",
+                                        paddingLeft: "1.5rem",
+                                        marginBottom: "100px"}}>
+                            <div style={{ display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center"}}>
+                                    <img src={pwcLogo} alt="logo" style={{marginTop: "1.5rem",
+                                        marginBottom: "1.5rem", height: "100px", width: "auto",
+                                        alignSelf: "center"}}></img>
+                                <Typography variant="h6" component="h6"
+                                            style={{ marginBottom: "20px"}}>
+                                    Login to continue
+                                </Typography>
+                            </div>
+                        <form noValidate autoComplete="off">
+                            <TextField
+                                label="Email"
+                                type="email"
+                                fullWidth
+                                required
+                                margin="normal"
+                                variant="outlined"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <TextField
+                                label="Password"
+                                type="password"
+                                fullWidth
+                                required
+                                margin="normal"
+                                variant="outlined"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button
+                                onClick={handleSubmit}
+                                color="primary"
+                                variant="contained"
+                                fullWidth
+                                style={{ marginTop: "20px",
+                                        height: "45px"}}
+                            >
+                                Login
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Container>
     );
 }
 
