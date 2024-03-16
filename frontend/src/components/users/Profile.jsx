a3b364196f86ff4f9e910577dd7e835c4188f178
Billy <ryotabilly@gmail.com>
2024-03-16 00:39:26 -0700

    feat(review):add a default user avatar

--- frontend/src/components/users/Profile.jsx
+++ frontend/src/components/users/Profile.jsx
@@ -1,35 +1,49 @@
 import { Avatar } from "@mui/material";
 
 export default function Profile({ user }) {
-    function stringToColor(string) {
-        let hash = 0;
-        let i;
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
 
-        for (i = 0; i < string.length; i += 1) {
-            hash = string.charCodeAt(i) + ((hash << 5) - hash);
-        }
-
-        let color = "#";
-        for (i = 0; i < 3; i += 1) {
-            const value = (hash >> (i * 8)) & 0xff;
-            color += `00${value.toString(16)}`.slice(-2);
-        }
-
-        return color;
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
 
-    function stringAvatar(name) {
-        return {
-            sx: {
-                bgcolor: stringToColor(name),
-                width: 32,
-                height: 32,
-                fontSize: 14,
-                margin: 0,
-            },
-            children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-        };
+    let color = "#";
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
     }
 
-    return <Avatar {...stringAvatar(`${user.firstName} ${user.lastName}`)} />;
+    return color;
+  }
+
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+        width: 32,
+        height: 32,
+        fontSize: 14,
+        margin: 0,
+      },
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    };
+  }
+
+  return user ? (
+    <Avatar {...stringAvatar(`${user.firstName} ${user.lastName}`)} />
+  ) : (
+    <Avatar
+      sx={{
+        bgcolor: "grey",
+        width: 32,
+        height: 32,
+        fontSize: 14,
+        margin: 0,
+      }}
+    >
+      {"U"}
+    </Avatar>
+  );
 }
