662bf72cd4f1a4017bf441cedf4c36de13463a48
sidaarth <sidaarthsanthosh@gmail.com>
2024-03-27 05:25:52 -0700

    - fix to checking incident details in report view
    - added call to addTaskModal in report view

--- frontend/src/pages/incident/report/index.jsx
+++ frontend/src/pages/incident/report/index.jsx
@@ -5,6 +5,9 @@ import useTasks from "../../../hooks/useTasks";
 import Profile from "../../../components/users/Profile";
 import IncidentDetailModal from "../../../components/incident/IncidentDetailModal";
 import { useBoard } from "../../../context/BoardContext";
+import AddTaskModal from "../../../components/incident/AddTaskModal";
+import useForm from "../../../hooks/useForm";
+import useAxios from "../../../hooks/useAxios";
 
 function IncidentReport() {
     const { tasks } = useTasks();
@@ -12,7 +15,7 @@ function IncidentReport() {
     const [selectedIncident, setSelectedIncident] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const { statuses } = useBoard();
-
+    const { sendRequest } = useAxios();
 
     const styles = {
         styleHeader: {
@@ -51,8 +54,65 @@ function IncidentReport() {
         setRows(Object.values(tasks).flat());
     }, [tasks]);
 
+
+    const [addModal, setAddModal] = useState(false);
+    const toggleAddModal = () => setAddModal(!addModal);
+
+    const handleAddTask = async (task) => {
+        const directMapping = {
+          time_of_incident: "incidentDate",
+          category: "incidentCategory",
+          employees_involved: "employeesInvolved",
+        };
+    
+        const incident = {
+          reporter: user.id,
+          incidentDate: task.time_of_incident,
+          incidentCategory: task.category,
+          employeesInvolved: task.employees_involved,
+          customFields: [],
+          statusId: statuses.find((status) => status.name === "Pending Review")?.id,
+        };
+    
+        for (const key in task) {
+          if (
+            Object.prototype.hasOwnProperty.call(task, key) &&
+            !directMapping[key]
+          ) {
+            incident.customFields.push({
+              fieldName: key,
+              value: task[key],
+            });
+          }
+        }
+        await sendRequest({ url: "/incidents", method: "POST", body: incident });
+        fetchTasks();
+    };
+
+    const {forms, fetchForms, groupedByRows, sortedRows} = useForm();
+    const [fields, setFields] = useState({});
+    useEffect(() => {
+        fetchForms();
+    }, []);
+
+    useEffect(() => {
+        // TODO: remove this when the form is selected from the form list
+        setFields(forms["GZ4tf8bErd3rZ9YizFOu"]?.fields);
+    }, [forms]);
+
+    const handleSort = () => {
+        return sortedRows(groupedByRows(fields));
+    };
+    
     return (
         <>
+            <AddTaskModal
+                open={addModal}
+                onClose={toggleAddModal}
+                handleAddTask={handleAddTask}
+                field={fields}
+                sortedRows={handleSort}
+            />
             <Typography variant="h4" component="h4"
                         style={{ fontFamily: "ITC Charter", marginLeft: "3rem", fontWeight: "bold",
                         marginTop: "1.8rem", color: "#2D2D2D"}}>
@@ -60,15 +120,15 @@ function IncidentReport() {
             </Typography>
             <div style={{marginRight:"3rem", marginLeft: "3rem"}}>
                 <div style={{display: "flex", justifyContent: 'flex-end'}}>
-                    <Button variant="contained" onClick={() => {}}
+                    <Button variant="contained" onClick={toggleAddModal}
                     style={{display: 'flex', justifyContent: 'flex-end', marginBottom: "1rem",
                         fontWeight: "bold"}}>
                         Add Incident
                     </Button>
                 </div>
                 <IncidentDataGrid rows={rows} columns={columns} onRowClick={handleRowClick}/>
-                    {isModalOpen && (
-                        <IncidentDetailModal open={isModalOpen} incidentId={selectedIncident.id} onClose={handleCloseModal} />
+                    {isModalOpen && selectedIncident && (
+                        <IncidentDetailModal open={isModalOpen} selectedIncident={selectedIncident} incidentId={selectedIncident.id} onClose={handleCloseModal} />
                     )}
             </div>
         </>
