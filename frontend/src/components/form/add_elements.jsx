8d29a35b43696d4f6fa37d6212af7fa8a648cc59
Billy <ryotabilly@gmail.com>
2024-04-03 14:56:03 -0700

    pipe

--- frontend/src/components/form/add_elements.jsx
+++ frontend/src/components/form/add_elements.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FIELD_TYPES } from './form_data';
 import AddInputField from './AddInputField';
 import AddSelectionField from './AddSelectionField';
-import AddFileField from './AddFileField';
+import AddAutoInputField from './AddAutoInputField';
 export const FIELD_ADD_FORM = {
   [FIELD_TYPES.TEXT_BOX]: ({onTitleChange, onDescriptionChange, onPlaceHolderChange, onRequiredChange, initialTitle, initialDescription, initialPlaceHolder, initialRequired})=> (
     <AddInputField type={FIELD_TYPES.TEXT_BOX} onTitleChange={onTitleChange} onDescriptionChange={onDescriptionChange} onPlaceHolderChange={onPlaceHolderChange} onRequiredChange={onRequiredChange} initialTitle={initialTitle} initialDescription={initialDescription} initialPlaceHolder={initialPlaceHolder} initialRequired={initialRequired}/>
@@ -16,7 +16,10 @@ export const FIELD_ADD_FORM = {
   [FIELD_TYPES.SELECTION_MULTI]: ({onTitleChange, onDescriptionChange, onOptionChange, initialTitle, initialDescription, initialOptions, initialRequired})=> (
     <AddSelectionField type={FIELD_TYPES.SELECTION_MULTI} onTitleChange={onTitleChange} onDescriptionChange={onDescriptionChange} onOptionChange={onOptionChange} initialTitle={initialTitle} initialDescription={initialDescription} initialOptions={initialOptions}/>
   ),
-  [FIELD_TYPES.SELECTION_SINGLE]: ({onTitleChange, onDescriptionChange, onOptionChange, initialTitle, initialDescription, initialOptions, initialRequired})=> (
+  [FIELD_TYPES.SELECTION_SINGLE]: ({onTitleChange, onDescriptionChange, onOptionChange, initialTitle, initialDescription, initialOptions, initialRequired,})=> (
     <AddSelectionField type={FIELD_TYPES.SELECTION_SINGLE} onTitleChange={onTitleChange} onDescriptionChange={onDescriptionChange} onOptionChange={onOptionChange} initialTitle={initialTitle} initialDescription={initialDescription} initialOptions={initialOptions} initialRequired={initialRequired}/>
   ),
+  [FIELD_TYPES.AI_TEXT]: ({onTitleChange, onDescriptionChange, onPlaceHolderChange, onRequiredChange, initialTitle, initialDescription, initialPlaceHolder, initialRequired, currentFields, onReferenceFieldChange, onPromptChange})=> (
+    <AddAutoInputField type={FIELD_TYPES.AI_TEXT} onTitleChange={onTitleChange} onDescriptionChange={onDescriptionChange} onPlaceHolderChange={onPlaceHolderChange} onRequiredChange={onRequiredChange} initialTitle={initialTitle} initialDescription={initialDescription} initialPlaceHolder={initialPlaceHolder} initialRequired={initialRequired} currentFields={currentFields} onReferenceFieldChange={onReferenceFieldChange} onPromptChange={onPromptChange}/>
+  )
 }
