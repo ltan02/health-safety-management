73a93e25293f1a2e5efb9b9fa72e09d7d912cdb0
shivamA16 <110695238+shivamA16@users.noreply.github.com>
2024-04-06 17:42:58 -0700

    Update main.py (#112)

--- ai/main.py
+++ ai/main.py
@@ -15,7 +15,6 @@ import re
 
 load_dotenv()
 google_credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
-
 if google_credentials_json:
     parsed_credentials = json.loads(google_credentials_json)
     credentials = service_account.Credentials.from_service_account_info(parsed_credentials)
@@ -25,12 +24,13 @@ if google_credentials_json:
 else:
     db = firestore.Client()
     aiplatform.init(project="pwc-project-b3778", location="us-west1")
-    client = bigquery.Client()
 
-app = FastAPI()
 doc_ref = db.collection('categories').document('categories')
 doc = doc_ref.get()
 categories = doc.to_dict().get('words', [])
+
+
+app = FastAPI()
 
 
 app.add_middleware(
@@ -69,9 +69,6 @@ class ChatPrompt(BaseModel):
     prompt: str
 
 
-model_basic = GenerativeModel("text-bison@001")
-
-
 @app.post("/categorize/")
 async def generate_text(incident_model: IncidentModel):
     incident = incident_model.incident
@@ -85,13 +82,10 @@ async def generate_text(incident_model: IncidentModel):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-model1 = GenerativeModel("gemini-1.0-pro")
-
-
 @app.post("/generate")
 async def talk(prompt_model: Prompt):
     try:
-        response = model1.generate_content(prompt_model.prompt)
+        response = model.generate_content(prompt_model.prompt)
         return {"response": response.text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -142,8 +136,8 @@ def newChat(chatsession):
     related question, first match their query to the closest category and then query that category.
 
     When the user asks a date or timeperiod related question, remember that the incidentDate field in the 'Data' field 
-    is an ISO formatted string with format yyyy-MM-dd'T'HH:mm, so use the LIKE() function for strings - do not cast them
-    to the DATE format. timeperiod comparisons should be made with numbers in string format. 
+    is an ISO formatted string with format yyyy-MM-dd'T'HH:mm, so use the SUBSTR() function to get dates - do not cast 
+    them to the DATE format. timeperiod comparisons should be made with numbers in string format. 
     
     For non-incident-related queries like 'What's the weather like today?', reply with: 'Query not related to incidents,
     please try again with an incident-related question.'
@@ -169,12 +163,38 @@ def newChat(chatsession):
     print("".join(t))
 
 
+def getUsers():
+    collection_ref = db.collection('users')
+    docs = collection_ref.stream()
+    documents_dict = {}
+
+    for doc in docs:
+        documents_dict[doc.id] = doc.to_dict()
+
+    return documents_dict
+
+
+def getStatuses():
+    collection_ref = db.collection('status')
+    docs = collection_ref.stream()
+    documents_dict = {}
+
+    for doc in docs:
+        documents_dict[doc.id] = doc.to_dict()
+
+    return documents_dict
+
+
+users = getUsers()
+statuses = getStatuses()
+
 def newChat2(chatsession):
     prompt = """
     You are now acting as a result presenting bot for PricewaterhouseCoopers (PwC), a global leader in professional 
     services, specializing in Assurance, Tax, and Advisory services. With the post-pandemic return to office, there is 
     a renewed focus on Health and Safety, critical for PwC's rapidly growing cloud engineering consulting practice in 
     Canada.
+    
     I will give you the results that I got by running an SQL query on data about incidents are PwC and your job is to 
     present them to the user in a nice and readable way. If, however, you receive the string - 'Query not related to 
     incidents, please try again with an incident-related question.', then return this string to the user as is. 
@@ -182,6 +202,9 @@ def newChat2(chatsession):
     Be sure to be highly accurate and incorporate the data given to you in your answers in the most effective way. 
     When done reading this prompt, respond with Done. The first part of the prompt is the user's query and the second 
     part of the prompt the data received by running the SQL query.
+    
+    If in the data you see any user ids - replace them with first and last names according to the dictionary - """ \
+    + str(users) + """. Also, replace any status ids with their names according to - """ + str(statuses) + """
     """
 
     t = []
@@ -246,4 +269,4 @@ async def get_chat_response(chat_prompt: ChatPrompt, uuid: str = Header(None)) -
 
 
 # if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8010)
+    # uvicorn.run(app, host="0.0.0.0", port=8010)
