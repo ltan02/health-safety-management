37e776afb8a801e90e3a3172d2ccf0c52f8cd39e
Billy <ryotabilly@gmail.com>
2024-04-08 14:09:47 -0700

    fix

--- frontend/src/pages/incident/report/index.jsx
+++ frontend/src/pages/incident/report/index.jsx
@@ -16,278 +16,310 @@ import AdminManagement from "../../admin/management";
 import { v4 as uuidv4 } from "uuid";
 
 function IncidentReport() {
-    const { filteredTasks, fetchTasks, setFilteredTasks, loading } = useTasks();
-    const [rows, setRows] = useState(Object.values(filteredTasks).flat());
-    const { user } = useAuthContext();
-    const [selectedIncident, setSelectedIncident] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const { statuses } = useBoard();
-    const { sendRequest } = useAxios();
-    const { states, transitions, fetchWorkflow } = useWorkflow();
-    const { fetchWorkflowNew, activeStateMap } = useWorkflowNew();
+  const { filteredTasks, fetchTasks, setFilteredTasks, loading } = useTasks();
+  const [rows, setRows] = useState(Object.values(filteredTasks).flat());
+  const { user } = useAuthContext();
+  const [selectedIncident, setSelectedIncident] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { statuses } = useBoard();
+  const { sendRequest } = useAxios();
+  const { states, transitions, fetchWorkflow } = useWorkflow();
+  const { fetchWorkflowNew, activeStateMap } = useWorkflowNew();
 
-    const [viewModalOpen, setViewModalOpen] = useState(false);
-    const [editModalOpen, setEditModalOpen] = useState(false);
+  const [viewModalOpen, setViewModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
 
-    const handleView = async () => {
-        setEditModalOpen(true);
-        setViewModalOpen(false);
+  const handleView = async () => {
+    setEditModalOpen(true);
+    setViewModalOpen(false);
+  };
+
+  useEffect(() => {
+    fetchWorkflow();
+    fetchWorkflowNew();
+  }, []);
+  const [commentData, setCommentData] = useState({});
+  const [employees, setEmployees] = useState([]);
+
+  const styles = {
+    styleHeader: {
+      backgroundColor: "red",
+    },
+  };
+
+  const columns = [
+    {
+      field: "incidentDate",
+      headerName: "Incident Date",
+      width: 150,
+      flex: 1,
+      headerClassName: styles.styleHeader,
+    },
+    {
+      field: "incidentCategory",
+      headerName: "Incident Category",
+      width: 300,
+      flex: 1,
+    },
+    {
+      field: "reporter",
+      headerName: "Reporter",
+      width: 200,
+      flex: 1,
+      renderCell: (params) => <Profile user={params.value} />,
+    },
+    {
+      field: "statusId",
+      headerName: "Status",
+      width: 400,
+      flex: 1,
+      renderCell: (params) =>
+        statuses.find((status) => status.id === params.value)?.name ??
+        "Unknown Status",
+    },
+    {
+      field: "reviewer",
+      headerName: "Reviewer",
+      width: 400,
+      flex: 1,
+      renderCell: (params) => <Profile user={params.value} />,
+    },
+  ];
+
+  const handleRowClick = (params) => {
+    setSelectedIncident(params.row);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedIncident(null);
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    setRows(Object.values(filteredTasks).flat());
+  }, [filteredTasks]);
+
+  useEffect(() => {
+    fetchWorkflow();
+  }, []);
+
+  const [addModal, setAddModal] = useState(false);
+  const toggleAddModal = () => setAddModal(!addModal);
+
+  const handleAddTask = async (task) => {
+    const directMapping = {
+      incidentDate: "incidentDate",
+      category: "incidentCategory",
+      employees_involved: "employeesInvolved",
     };
 
-    useEffect(() => {
-        fetchWorkflow();
-        fetchWorkflowNew();
-    }, []);
-    const [commentData, setCommentData] = useState({});
-    const [employees, setEmployees] = useState([]);
+    const startId = states.find((state) => state.data.label === "START")?.id;
+    const firstStateId = transitions.find(
+      (transition) => transition.source === startId
+    )?.target;
+    const statusId = activeStateMap[firstStateId]?.statusId;
 
-    const styles = {
-        styleHeader: {
-            backgroundColor: "red",
-        },
+    const incident = {
+      reporter: user.id,
+      incidentDate: task.incidentDate,
+      incidentCategory: task.category,
+      employeesInvolved: task.employees_involved,
+      customFields: [],
+      comments: [],
+      statusId: statusId,
     };
 
-    const columns = [
-        {
-            field: "incidentDate",
-            headerName: "Incident Date",
-            width: 150,
-            flex: 1,
-            headerClassName: styles.styleHeader,
-        },
-        { field: "incidentCategory", headerName: "Incident Category", width: 300, flex: 1 },
-        {
-            field: "reporter",
-            headerName: "Reporter",
-            width: 200,
-            flex: 1,
-            renderCell: (params) => <Profile user={params.value} />,
-        },
-        {
-            field: "statusId",
-            headerName: "Status",
-            width: 400,
-            flex: 1,
-            renderCell: (params) => statuses.find((status) => status.id === params.value)?.name ?? "Unknown Status",
-        },
-        {
-            field: "reviewer",
-            headerName: "Reviewer",
-            width: 400,
-            flex: 1,
-            renderCell: (params) => <Profile user={params.value} />,
-        },
-    ];
-
-    const handleRowClick = (params) => {
-        setSelectedIncident(params.row);
-        setIsModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setSelectedIncident(null);
-        setIsModalOpen(false);
-    };
-
-    useEffect(() => {
-        setRows(Object.values(filteredTasks).flat());
-    }, [filteredTasks]);
-
-    useEffect(() => {
-        fetchWorkflow();
-    }, []);
-
-    const [addModal, setAddModal] = useState(false);
-    const toggleAddModal = () => setAddModal(!addModal);
-
-    const handleAddTask = async (task) => {
-        const directMapping = {
-            incidentDate: "incidentDate",
-            category: "incidentCategory",
-            employees_involved: "employeesInvolved",
-        };
-
-        const startId = states.find((state) => state.data.label === "START")?.id;
-        const firstStateId = transitions.find((transition) => transition.source === startId)?.target;
-        const statusId = activeStateMap[firstStateId]?.statusId;
-
-        const incident = {
-            reporter: user.id,
-            incidentDate: task.incidentDate,
-            incidentCategory: task.category,
-            employeesInvolved: task.employees_involved,
-            customFields: [],
-            comments: [],
-            statusId: statusId,
-        };
-
-        for (const key in task) {
-            if (Object.prototype.hasOwnProperty.call(task, key) && !directMapping[key]) {
-                incident.customFields.push({
-                    fieldName: key,
-                    value: task[key],
-                });
-            }
-        }
-        await sendRequest({ url: "/incidents", method: "POST", body: incident });
-        fetchTasks();
-    };
-
-    const { forms, fetchForms, groupedByRows, sortedRows, activeForm } = useForm();
-    const [fields, setFields] = useState({});
-    useEffect(() => {
-        fetchForms();
-    }, []);
-
-    useEffect(() => {
-        setFields(activeForm?.fields);
-    }, [forms]);
-
-    const handleSort = () => {
-        return sortedRows(groupedByRows(fields));
-    };
-
-    const handleComment = () => {
-        const flatTasks = Object.values(filteredTasks).flat();
-        const initialCommentData = { ...commentData }; // shallow copy
-        if (flatTasks) {
-            flatTasks.map((task) => {
-                const newCommentData = initialCommentData;
-                task.comments.map((comment) => {
-                    if (!newCommentData[comment.id]) {
-                        newCommentData[comment.id] = [];
-                    }
-                    const data = {
-                        id: uuidv4(),
-                        comment,
-                        user: employees.filter((employee) => employee.id === comment.userId)[0],
-                    };
-                    const tempId = hasTempComment(newCommentData[comment.id], data);
-                    // this is to prevent duplicate temp comments
-                    if (tempId) {
-                        newCommentData[comment.id][tempId] = data;
-                    } else {
-                        newCommentData[comment.id].push(data);
-                    }
-                });
-
-                initialCommentData[task.id] = newCommentData[task.id];
-            });
-        }
-        setCommentData(initialCommentData);
-    };
-
-    const hasTempComment = (commentData, newComment) => {
-        const commentDataCopy = [...commentData];
-        let tempId = null;
-        commentDataCopy.map((data) => {
-            if (data.id.includes("temp") && data.comment.content === newComment.comment.content) {
-                tempId = data.id;
-                return;
-            }
+    for (const key in task) {
+      if (
+        Object.prototype.hasOwnProperty.call(task, key) &&
+        !directMapping[key]
+      ) {
+        incident.customFields.push({
+          fieldName: key,
+          value: task[key],
         });
-        return tempId;
+      }
+    }
+    await sendRequest({ url: "/incidents", method: "POST", body: incident });
+    fetchTasks();
+  };
+
+  const { forms, fetchForms, groupedByRows, sortedRows, activeForm } =
+    useForm();
+  const [fields, setFields] = useState({});
+  useEffect(() => {
+    fetchForms();
+  }, []);
+
+  useEffect(() => {
+    setFields(activeForm?.fields);
+  }, [forms]);
+
+  const handleSort = () => {
+    return sortedRows(groupedByRows(fields));
+  };
+
+  const handleComment = () => {
+    const flatTasks = Object.values(filteredTasks).flat();
+    const initialCommentData = { ...commentData }; // shallow copy
+    if (flatTasks) {
+      flatTasks.map((task) => {
+        const newCommentData = initialCommentData;
+        //since we do not have comment edit logic, if the size of the comments array is the same, we do not need to push the comment data
+        //this prevent us from pushing the same comment data multiple times
+        if (task.comments?.length === newCommentData[task.id]?.length) {
+          return;
+        }
+        task.comments.map((comment) => {
+          if (!newCommentData[comment.id]) {
+            newCommentData[comment.id] = [];
+          }
+          const data = {
+            id: uuidv4(),
+            comment,
+            user: employees.filter(
+              (employee) => employee.id === comment.userId
+            )[0],
+          };
+          const tempId = hasTempComment(newCommentData[comment.id], data);
+          if (tempId) {
+            newCommentData[comment.id][tempId] = data;
+          } else {
+            newCommentData[comment.id].push(data);
+          }
+        });
+        initialCommentData[task.id] = newCommentData[task.id];
+      });
+    }
+    setCommentData(initialCommentData);
+  };
+
+  const hasTempComment = (commentData, newComment) => {
+    const commentDataCopy = [...commentData];
+    let tempId = null;
+    commentDataCopy.map((data) => {
+      if (
+        data.id.includes("temp") &&
+        data.comment.content === newComment.comment.content
+      ) {
+        tempId = data.id;
+        return;
+      }
+    });
+    return tempId;
+  };
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      const res = await sendRequest({ url: "/users" });
+      setEmployees(res);
     };
+    fetchForms();
+    fetchEmployees();
+  }, []);
 
-    useEffect(() => {
-        const fetchEmployees = async () => {
-            const res = await sendRequest({ url: "/users" });
-            setEmployees(res);
-        };
-        fetchForms();
-        fetchEmployees();
-    }, []);
+  useEffect(() => {
+    handleComment();
+  }, [filteredTasks]);
 
-    useEffect(() => {
-        handleComment();
-    }, [filteredTasks]);
-
-    return (
-        <>
-            <AddTaskModal
-                open={addModal}
-                onClose={toggleAddModal}
-                handleAddTask={handleAddTask}
-                field={fields}
-                sortedRows={handleSort}
-                formName={activeForm?.name}
-            />
-            <Typography
-                variant="h4"
-                component="h4"
-                style={{
-                    fontFamily: "ITC Charter",
-                    marginLeft: "3rem",
-                    fontWeight: "bold",
-                    marginTop: "1.8rem",
-                    color: "#2D2D2D",
-                }}
+  return (
+    <>
+      <AddTaskModal
+        open={addModal}
+        onClose={toggleAddModal}
+        handleAddTask={handleAddTask}
+        field={fields}
+        sortedRows={handleSort}
+        formName={activeForm?.name}
+      />
+      <Typography
+        variant="h4"
+        component="h4"
+        style={{
+          fontFamily: "ITC Charter",
+          marginLeft: "3rem",
+          fontWeight: "bold",
+          marginTop: "1.8rem",
+          color: "#2D2D2D",
+        }}
+      >
+        Incident Reports
+      </Typography>
+      <div style={{ marginRight: "3rem", marginLeft: "3rem" }}>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          {!loading && (
+            <Button
+              variant="contained"
+              onClick={toggleAddModal}
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                marginBottom: "1rem",
+                fontWeight: "bold",
+              }}
             >
-                Incident Reports
-            </Typography>
-            <div style={{ marginRight: "3rem", marginLeft: "3rem" }}>
-                <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    {!loading && (
-                        <Button
-                            variant="contained"
-                            onClick={toggleAddModal}
-                            style={{
-                                display: "flex",
-                                justifyContent: "flex-end",
-                                marginBottom: "1rem",
-                                fontWeight: "bold",
-                            }}
-                        >
-                            Add Incident
-                        </Button>
-                    )}
-                </div>
-                {loading && (
-                    <Box
-                        sx={{
-                            position: "fixed",
-                            top: 0,
-                            left: 0,
-                            width: "100%",
-                            height: "100%",
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            zIndex: 1500,
-                            pointerEvents: "none",
-                        }}
-                    >
-                        <CircularProgress />
-                    </Box>
-                )}
+              Add Incident
+            </Button>
+          )}
+        </div>
+        {loading && (
+          <Box
+            sx={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              zIndex: 1500,
+              pointerEvents: "none",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        )}
 
-                {!loading && <IncidentDataGrid rows={rows} columns={columns} onRowClick={handleRowClick} />}
-                {isModalOpen && selectedIncident && (
-                    <IncidentDetailModal
-                        open={isModalOpen}
-                        selectedIncident={selectedIncident}
-                        incidentId={selectedIncident}
-                        onClose={handleCloseModal}
-                        setTasks={setFilteredTasks}
-                        transitions={transitions}
-                        setViewModalOpen={setViewModalOpen}
-                        commentData={commentData}
-                        setCommentData={setCommentData}
-                    />
-                )}
-            </div>
-            {viewModalOpen && (
-                <ViewWorkflowModal
-                    open={viewModalOpen}
-                    handleClose={() => {
-                        setViewModalOpen(false);
-                    }}
-                    handleEdit={handleView}
-                />
-            )}
-            {editModalOpen && <AdminManagement open={editModalOpen} handleClose={() => setEditModalOpen(false)} />}
-        </>
-    );
+        {!loading && (
+          <IncidentDataGrid
+            rows={rows}
+            columns={columns}
+            onRowClick={handleRowClick}
+          />
+        )}
+        {isModalOpen && selectedIncident && (
+          <IncidentDetailModal
+            open={isModalOpen}
+            selectedIncident={selectedIncident}
+            incidentId={selectedIncident}
+            onClose={handleCloseModal}
+            setTasks={setFilteredTasks}
+            transitions={transitions}
+            setViewModalOpen={setViewModalOpen}
+            commentData={commentData}
+            setCommentData={setCommentData}
+          />
+        )}
+      </div>
+      {viewModalOpen && (
+        <ViewWorkflowModal
+          open={viewModalOpen}
+          handleClose={() => {
+            setViewModalOpen(false);
+          }}
+          handleEdit={handleView}
+        />
+      )}
+      {editModalOpen && (
+        <AdminManagement
+          open={editModalOpen}
+          handleClose={() => setEditModalOpen(false)}
+        />
+      )}
+    </>
+  );
 }
 
 export default IncidentReport;
