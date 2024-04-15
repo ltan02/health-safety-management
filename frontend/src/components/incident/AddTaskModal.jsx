cdd5c0aff96d06b3ab411bf2f46af5d48f938903
Billy <ryotabilly@gmail.com>
2024-04-15 16:57:24 -0700

    add warning message before closing

--- frontend/src/components/incident/AddTaskModal.jsx
+++ frontend/src/components/incident/AddTaskModal.jsx
@@ -1,50 +1,93 @@
-import { Box, Modal } from "@mui/material";
+import { Box, Modal, Typography, Button } from "@mui/material";
+import { useState } from "react";
 import PreviewForm from "../form/PreviewForm";
+import { set } from "lodash";
 
 const modalStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "auto",
-    bgcolor: "background.paper",
-    boxShadow: 24,
-    p: 7,
-    borderRadius: 2,
-    overflow: "auto",
-    maxHeight: "90%",
-    display: "flex",
-    flexDirection: "column",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 7,
+  borderRadius: 2,
+  overflow: "auto",
+  maxHeight: "90%",
+  display: "flex",
+  flexDirection: "column",
 };
 
-function AddTaskModal({ open, onClose, handleAddTask, field, sortedRows, formName }) {
-    const handleSubmit = (field) => {
-        handleAddTask(field);
-        onClose();
-    };
-    return (
-        <Modal
-            open={open}
-            onClose={() => {
+function AddTaskModal({
+  open,
+  onClose,
+  handleAddTask,
+  field,
+  sortedRows,
+  formName,
+}) {
+  const [cancelModal, setCancelModal] = useState(false);
+  const handleSubmit = (field) => {
+    handleAddTask(field);
+    onClose();
+  };
+  const handleClose = () => {
+    setCancelModal(true);
+    console.log("cancel");
+  };
+  return (
+    <Modal
+      open={open}
+      onClose={() => {
+        handleClose();
+      }}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={modalStyle}>
+        {/*<Typography id="modal-modal-title" variant="h6" component="h2" sx={{ mb: 2 }}>*/}
+        {/*    Report New Incident*/}
+        {/*</Typography>*/}
+        <PreviewForm
+          fields={field}
+          sortedRows={sortedRows}
+          handleSubmit={handleSubmit}
+          onClose={() => setCancelModal(true)}
+          formName={formName}
+        />
+        <Modal open={cancelModal} onClose={() => setCancelModal(false)}>
+          <Box sx={modalStyle}>
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+              sx={{ mb: 2 }}
+            >
+              Are you sure you want to cancel?
+            </Typography>
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={() => {
+                setCancelModal(false);
                 onClose();
-            }}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box sx={modalStyle}>
-                {/*<Typography id="modal-modal-title" variant="h6" component="h2" sx={{ mb: 2 }}>*/}
-                {/*    Report New Incident*/}
-                {/*</Typography>*/}
-                <PreviewForm
-                    fields={field}
-                    sortedRows={sortedRows}
-                    handleSubmit={handleSubmit}
-                    onClose={onClose}
-                    formName={formName}
-                />
-            </Box>
+              }}
+            >
+              Yes
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => setCancelModal(false)}
+            >
+              No
+            </Button>
+          </Box>
         </Modal>
-    );
+      </Box>
+    </Modal>
+  );
 }
 
 export default AddTaskModal;
